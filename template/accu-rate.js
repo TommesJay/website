e7c324344d86d0961827f47a894bc595773d9537
Florian Sesser <fs@accu-rate.de>
2014-06-12 12:45:20 +0200

    Fix #7: Add querySelectorAll() to old IE (tested on IE5 and IE7)

--- template/accu-rate.js
+++ template/accu-rate.js
@@ -1,4 +1,9 @@
 /* Sehr dumme Spambots blenden. */
+if(typeof(window.document.querySelectorAll) == "undefined") {
+    // Thanks to http://www.codecouch.com/2012/05/adding-document-queryselectorall-support-to-ie-7/
+    // IE7 support for querySelectorAll in 274 bytes. Supports multiple / grouped selectors and the attribute selector with a "for" attribute. http://www.codecouch.com/
+(function(d,s){d=document,s=d.createStyleSheet();d.querySelectorAll=function(r,c,i,j,a){a=d.all,c=[],r=r.replace(/\[for\b/gi,'[htmlFor').split(',');for(i=r.length;i--;){s.addRule(r[i],'k:v');for(j=a.length;j--;)a[j].currentStyle.k&&c.push(a[j]);s.removeRule(0)}return c}})()
+}
 var addrViews = window.document.querySelectorAll(".mailadresse");
 for (var i = 0; i < addrViews.length; i++) {
     var tmp1 = addrViews[i].attributes.getNamedItem("data-to").value;
